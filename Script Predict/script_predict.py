9474bec581b6efc3622825a37ef1ba82a994f205
dorianGT <doriangrouteaupro@gmail.com>
2024-05-24 21:23:20 +0200

    Update

--- Script Predict/script_predict.py
+++ Script Predict/script_predict.py
@@ -166,15 +166,15 @@ def get_data_books(chemin):
     fichiers = os.listdir(chemin)
     books = []
     y = []
-
+    filenames = []
     for fichier in fichiers:
         chemin_fichier = os.path.join(chemin, fichier)
         texte,annee = get_data_book(fichier,chemin_fichier)
         if(annee != None):
             books.append(texte)
             y.append(annee)
-
-    return books, y
+            filenames.append(fichier)
+    return books, y,filenames
 
 # Fonction pour diviser une liste en segments avec padding
 def diviser_liste(liste, labels, taille):
@@ -270,17 +270,23 @@ def charger_donnees_de_fichier(path):
         print(f"Erreur lors de la lecture du fichier : {e}")
         return None
 
-# Programme principal
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Veuillez fournir le chemin vers le fichier de données ou le dossier en argument.")
-        sys.exit(1)
+    model_path = ""
+    # Demander le chemin du modèle jusqu'à ce qu'un chemin valide soit fourni
+    while not os.path.isfile(model_path):
+        model_path = input("Veuillez fournir le chemin vers le fichier du modèle : ")
+        if not os.path.isfile(model_path):
+            print("Le chemin spécifié n'est pas un fichier valide.")
 
-    chemin = sys.argv[1]
-    if len(sys.argv) == 3:
-        model_path = sys.argv[2]
     # Chargement du modèle
     model = load_model(model_path, custom_objects={'accuracy': accuracy_with_tolerance(25)})
+    
+    chemin = ""
+    # Demander le chemin du fichier ou du dossier jusqu'à ce qu'un chemin valide soit fourni
+    while not os.path.exists(chemin):
+        chemin = input("Veuillez fournir le chemin vers le fichier de données ou le dossier : ")
+        if not os.path.exists(chemin):
+            print("Le chemin spécifié n'existe pas.")
 
     # Afficher le résumé du modèle
     model.summary()
@@ -292,81 +298,81 @@ if __name__ == "__main__":
     # Constantes
     max_len = input_shape[0][1]
 
-    if os.path.isfile(chemin) or os.path.isdir(chemin):
-        # Si c'est un fichier, chargez les données
-        if os.path.isfile(chemin):
-            fichier = os.path.basename(chemin)
-            texte,annee = get_data_book(fichier,chemin)
-            X_test, y_test = [texte],[annee]
+    # Si c'est un fichier, chargez les données
+    if os.path.isfile(chemin):
+        fichier = os.path.basename(chemin)
+        texte,annee = get_data_book(fichier,chemin)
+        X_test, y_test, filenames = [texte],[annee],[fichier]
 
-        # Si c'est un dossier
-        elif os.path.isdir(chemin):
-            X_test, y_test = get_data_books(chemin)
-            
-        sequences_testA, sequences_testB, sequences_testC, sequences_testD = tokenizer.tokenize(X_test)
-        data_testA, y_test, sequence_count_book_test = diviser_liste(sequences_testA, y_test, max_len)
-        data_testB = diviser_liste2(sequences_testB, max_len)
-        data_testC = diviser_liste2(sequences_testC, max_len)
-        data_testD = diviser_liste2(sequences_testD, max_len)
-        test = [data_testA, data_testB, data_testC, data_testD]
-        predictions = model.predict(test)
-
-        loss_test, mae_test,accuracy_test = model.evaluate(test, y_test)
-
-        predicted_book_date = np.zeros(len(sequence_count_book_test))
-        true_book_date = np.zeros(len(sequence_count_book_test))
-        diff_total = 0
-        ind = 0
-        current = 0
-        # Parcours de toutes les séquences
-        for nb in sequence_count_book_test: 
-            dates = []  # Liste pour stocker les dates prédites pour chaque séquence
-
-            # Collecte des prédictions pour chaque séquence
-            for i in range(nb):
-                dates.append(predictions[current])  # Ajouter la prédiction actuelle à la liste des dates
-                current += 1  # Incrémenter l'indice pour la prochaine prédiction
+    # Si c'est un dossier
+    elif os.path.isdir(chemin):
+        X_test, y_test, filenames = get_data_books(chemin)
         
-            # Retirer les valeurs extrêmes
-            if len(dates) > 2:
-                dates.remove(min(dates))  # Supprime la valeur minimale
-                dates.remove(max(dates))  # Supprime la valeur maximale
+    sequences_testA, sequences_testB, sequences_testC, sequences_testD = tokenizer.tokenize(X_test)
+    data_testA, y_test, sequence_count_book_test = diviser_liste(sequences_testA, y_test, max_len)
+    data_testB = diviser_liste2(sequences_testB, max_len)
+    data_testC = diviser_liste2(sequences_testC, max_len)
+    data_testD = diviser_liste2(sequences_testD, max_len)
+    test = [data_testA, data_testB, data_testC, data_testD]
+    predictions = model.predict(test)
 
-            # Calculer la moyenne des dates prédites (sans les valeurs extrêmes)
-            date = sum(dates) / len(dates) if dates else 0
-            # date = statistics.median(dates)
-            # Mettre à jour les tableaux avec la date moyenne prédite et la vérité terrain
-            predicted_book_date[ind] = int(date)
-            true_book_date[ind] = y_test[current - 1]
+    loss_test, mae_test,accuracy_test = model.evaluate(test, y_test)
 
-            # Calculer la différence absolue entre la moyenne prédite et la vérité terrain
-            diff = abs(date - y_test[current - 1])
-            diff_total += diff
+    predicted_book_date = np.zeros(len(sequence_count_book_test))
+    true_book_date = np.zeros(len(sequence_count_book_test))
+    diff_total = 0
+    ind = 0
+    current = 0
+    # Parcours de toutes les séquences
+    for nb in sequence_count_book_test: 
+        dates = []  # Liste pour stocker les dates prédites pour chaque séquence
 
-            ind += 1
+        # Collecte des prédictions pour chaque séquence
+        for i in range(nb):
+            dates.append(predictions[current])  # Ajouter la prédiction actuelle à la liste des dates
+            current += 1  # Incrémenter l'indice pour la prochaine prédiction
+    
+        # Retirer les valeurs extrêmes
+        if len(dates) > 2:
+            dates.remove(min(dates))  # Supprime la valeur minimale
+            dates.remove(max(dates))  # Supprime la valeur maximale
 
-        for i in range(len(predicted_book_date)):
-            print(f"Prédiction {i} : {predicted_book_date[i]}, Réel : {true_book_date[i]}")
+        # Calculer la moyenne des dates prédites (sans les valeurs extrêmes)
+        date = sum(dates) / len(dates) if dates else 0
+        # date = statistics.median(dates)
+        # Mettre à jour les tableaux avec la date moyenne prédite et la vérité terrain
+        predicted_book_date[ind] = int(date)
+        true_book_date[ind] = y_test[current - 1]
 
-        # Calculer la MAE pour les livres
-        mae_book = diff_total / len(sequence_count_book_test)
+        # Calculer la différence absolue entre la moyenne prédite et la vérité terrain
+        diff = abs(date - y_test[current - 1])
+        diff_total += diff
 
-        tolerance = 25  # Tolérance définie pour considérer les prédictions comme correctes
-        within_tolerance_count = 0  # Compteur pour les prédictions dans la tolérance
+        ind += 1
 
-        for i in range(len(predicted_book_date)):
-            # Vérification si la différence entre la date réelle et la date prédite est dans la tolérance
-            if abs(round(true_book_date[i]) - predicted_book_date[i]) <= tolerance:
-                within_tolerance_count += 1
+    for i in range(len(predicted_book_date)):
+        print(f"{filenames[i]}, Prédiction : {predicted_book_date[i]}, Réel : {true_book_date[i]}")
 
-        # Calcul de l'exactitude dans la tolérance
-        accuracy_within_tolerance_book = (within_tolerance_count / len(predicted_book_date)) * 100
+    # Calculer la MAE pour les livres
+    mae_book = diff_total / len(sequence_count_book_test)
 
-        # Affichage des résultats
-        print(f"MSE (Mean Square Error): {loss_test:.4f}")
-        print(f"MAE (Mean Absolute Error): {mae_test:.4f}")
-        print(f"Sequence Accuracy within tolerance: {accuracy_test:.4f}")
-        print(f"MAE Book (Mean Absolute Error): {mae_book[0]:.4f}")
-        print(f"Book Accuracy within tolerance: {accuracy_within_tolerance_book:.2f}%")
-    else:
-        print("Le chemin spécifié n'est ni un fichier ni un dossier.")
+    tolerance = 25  # Tolérance définie pour considérer les prédictions comme correctes
+    within_tolerance_count = 0  # Compteur pour les prédictions dans la tolérance
+
+    for i in range(len(predicted_book_date)):
+        # Vérification si la différence entre la date réelle et la date prédite est dans la tolérance
+        if abs(round(true_book_date[i]) - predicted_book_date[i]) <= tolerance:
+            within_tolerance_count += 1
+
+    # Calcul de l'exactitude dans la tolérance
+    accuracy_within_tolerance_book = (within_tolerance_count / len(predicted_book_date)) * 100
+
+    # Affichage des résultats
+    print("-"*20)
+    print("RESULTATS")
+    print("-"*20)
+    print(f"MSE (Mean Square Error): {loss_test:.4f}")
+    print(f"MAE (Mean Absolute Error): {mae_test:.4f}")
+    print(f"Sequence Accuracy within tolerance: {accuracy_test:.4f}")
+    print(f"MAE Book (Mean Absolute Error): {mae_book[0]:.4f}")
+    print(f"Book Accuracy within tolerance: {accuracy_within_tolerance_book:.2f}%")
